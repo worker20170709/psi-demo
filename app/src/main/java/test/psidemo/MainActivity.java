4605b56a4b8944b82f5165775e9d818b1a3c35b3
tyh <tyh1984@live.com>
2017-07-08 14:20:20 +0800

    Add Google Maps

--- app/src/main/java/test/psidemo/MainActivity.java
+++ app/src/main/java/test/psidemo/MainActivity.java
@@ -3,11 +3,34 @@ package test.psidemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sg = new LatLng(1.36, 103.83);
+        mMap.addMarker(new MarkerOptions().position(sg).title("Marker on SG"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sg));
+    }
+
 }
